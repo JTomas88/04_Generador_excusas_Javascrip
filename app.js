d9c95e409c8d96a121bc225a02043b7b2cfaf3e8
JTomas88 <juan.tomas88@hotmail.com>
2024-09-06 13:13:23 +0200

    se quitan importaciones

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 function generador() {
   //write your code here
